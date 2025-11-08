d5b6904f8c57c683ac6ff974e89c127dc1315364
“mikeburf” <mjb2632@utulsa.edu>
2025-11-07 18:09:27 -0600

    Set up to work for one wheel

--- motors.py
+++ motors.py
@@ -2,24 +2,37 @@ import RPi.GPIO as GPIO
 from time import sleep
 from time import time
 
-PIN_LEFT = 18
-PIN_RIGHT = 17
+PIN_PWM = 18
+PIN_CONTROL_A = 17
+PIN_CONTROL_B = 27
 
 class MotorController:
     def __enter__(self):
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(PIN_LEFT, GPIO.OUT)
-        GPIO.setup(PIN_RIGHT, GPIO.OUT)
+        GPIO.setup(PIN_PWM, GPIO.OUT)
+        self.pwm = GPIO.PWM(PIN_PWM, 1000)
+        self.pwm.start(50)
 
-        self.left = GPIO.PWM(PIN_LEFT, 1000)
-        self.right = GPIO.PWM(PIN_RIGHT, 1000)
-        self.left.start(50)
-        self.right.start(50)
         return self
+    
+    def brake(self):
+        GPIO.output(PIN_CONTROL_A, GPIO.LOW)
+        GPIO.output(PIN_CONTROL_B, GPIO.LOW)
+    
+    def float(self):
+        GPIO.output(PIN_CONTROL_A, GPIO.HIGH)
+        GPIO.output(PIN_CONTROL_B, GPIO.HIGH)
+
+    def forward(self):
+        GPIO.output(PIN_CONTROL_A, GPIO.HIGH)
+        GPIO.output(PIN_CONTROL_B, GPIO.LOW)
+
+    def reverse(self):
+        GPIO.output(PIN_CONTROL_A, GPIO.LOW)
+        GPIO.output(PIN_CONTROL_B, GPIO.HIGH)
     
     def __exit__(self):
         self.left.stop()
-        self.right.stop()
         GPIO.cleanup()
 
     

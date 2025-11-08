82750311aa0824b6c2d5c37e3d6e3a5712a60354
“mikeburf” <mjb2632@utulsa.edu>
2025-11-07 18:11:52 -0600

    Rename symbols

--- motors.py
+++ motors.py
@@ -2,34 +2,34 @@ import RPi.GPIO as GPIO
 from time import sleep
 from time import time
 
-PIN_PWM = 18
-PIN_CONTROL_A = 17
-PIN_CONTROL_B = 27
+PIN_R_PWM = 18
+PIN_R_CNTRL_1 = 17
+PIN_R_CNTRL_2 = 27
 
 class MotorController:
     def __enter__(self):
         GPIO.setmode(GPIO.BCM)
-        GPIO.setup(PIN_PWM, GPIO.OUT)
-        self.pwm = GPIO.PWM(PIN_PWM, 1000)
+        GPIO.setup(PIN_R_PWM, GPIO.OUT)
+        self.pwm = GPIO.PWM(PIN_R_PWM, 1000)
         self.pwm.start(50)
 
         return self
     
     def brake(self):
-        GPIO.output(PIN_CONTROL_A, GPIO.LOW)
-        GPIO.output(PIN_CONTROL_B, GPIO.LOW)
+        GPIO.output(PIN_R_CNTRL_1, GPIO.LOW)
+        GPIO.output(PIN_R_CNTRL_2, GPIO.LOW)
     
     def float(self):
-        GPIO.output(PIN_CONTROL_A, GPIO.HIGH)
-        GPIO.output(PIN_CONTROL_B, GPIO.HIGH)
+        GPIO.output(PIN_R_CNTRL_1, GPIO.HIGH)
+        GPIO.output(PIN_R_CNTRL_2, GPIO.HIGH)
 
     def forward(self):
-        GPIO.output(PIN_CONTROL_A, GPIO.HIGH)
-        GPIO.output(PIN_CONTROL_B, GPIO.LOW)
+        GPIO.output(PIN_R_CNTRL_1, GPIO.HIGH)
+        GPIO.output(PIN_R_CNTRL_2, GPIO.LOW)
 
     def reverse(self):
-        GPIO.output(PIN_CONTROL_A, GPIO.LOW)
-        GPIO.output(PIN_CONTROL_B, GPIO.HIGH)
+        GPIO.output(PIN_R_CNTRL_1, GPIO.LOW)
+        GPIO.output(PIN_R_CNTRL_2, GPIO.HIGH)
     
     def __exit__(self):
         self.left.stop()

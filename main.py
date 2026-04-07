29a0545e67775abf2a09f7c59e6769518cb7e3be
mikeburf <mjb2632@utulsa.edu>
2026-04-06 19:31:31 -0500

    Update state machine to work for both wheels and update turning logic

--- main.py
+++ main.py
@@ -4,45 +4,24 @@ import curses
 
 def main(stdscr: curses.window):
     motors = MotorController()
-    motors.set_speed(25)
+    motors.update_speed(25)
 
     stdscr.nodelay(True)
-    stdscr.timeout(50)
+    #stdscr.timeout(50)
 
     stdscr.addstr(0, 0, "Robot running. wasd to move, q to quit.")
-    time_init = perf_counter()
     while True:
         key = stdscr.getch()
-
-        fw = 0
-        lr = 0
-        if key == ord("q"): break
-        if key == ord("w"): fw += 1
-        if key == ord("s"): fw -= 1
-        if key == ord("a"): lr = -1
-        if key == ord("d"): lr += 1
-
-        stdscr.move(1, 0)
-        stdscr.clrtoeol()
-        if lr == -1:
-            stdscr.addstr(1, 0, "Moving left") 
-            motors.left()
-        elif lr == 1: 
-            stdscr.addstr(1, 0, "Moving right") 
-            motors.right()
-        elif fw == 1: 
-            stdscr.addstr(1, 0, "Moving forward") 
-            motors.forward()
-        elif fw == -1: 
-            stdscr.addstr(1, 0, "Moving backward") 
-            motors.reverse()
-
-        elif fw == 0 and lr == 0:
-            stdscr.addstr(1, 0, "Braking") 
-            motors.brake()
-
-        #curses.flushinp()
-        #motors.update_both()
+        
+        if key != -1:
+            stdscr.move(1, 0)
+            stdscr.clrtoeol()   
+            if key == ord("q"): break
+            elif key == ord("w"): motors.forward()
+            elif key == ord("s"): motors.reverse()
+            elif key == ord("a"): motors.left()
+            elif key == ord("d"): motors.right()
+            else: motors.brake()
 
     motors.cleanup()
 
--- motors.py
+++ motors.py
@@ -1,11 +1,10 @@
-
-from motor_state import *
-
 # import RPi.GPIO if it exists: otherwise simulate it
 try:
     import RPi.GPIO as GPIO
 except ImportError:
     import FakeRPi.GPIO as GPIO
+
+from time import sleep, monotonic
 
 PIN_R_PWM = 18
 PIN_R_CNTRL_1 = 17
@@ -14,6 +13,9 @@ PIN_R_CNTRL_2 = 27
 PIN_L_PWM = 12
 PIN_L_CNTRL_1 = 5
 PIN_L_CNTRL_2 = 6
+
+TURN_SLOW_WHEEL_SPEED_MULT = 0.2
+SAFETY_BRAKE_TIME = 0.1
 
 class Motor:
     def __init__(self, pin_pwm, pin_cntrl_1, pin_cntrl_2):
@@ -28,51 +30,25 @@ class Motor:
         self.pwm = GPIO.PWM(self.pin_pwm, 1000)
         self.pwm.start(0)
 
-        self.state = MotorStateController()
-
         self.float()
 
-    def update(self):
-        pass
-        #dir = self.state.update_and_get()
-
-        #match dir:
-        #    case Direction.BRAKE: self.__brake()
-        #    case Direction.FLOAT: self.__float()
-        #    case Direction.FORWARD: self.__forward()
-        #    case Direction.REVERSE: self.__reverse()
-
-    def set_speed(self, speed):
-        if speed is None: return
-        speed = min(speed, 100) #A simple clamp
-        speed = max(speed, 0)
-        self.pwm.ChangeDutyCycle(speed)
-
-    #def brake(self):
-    #    self.state.update(Direction.BRAKE)
+    def set_dc(self, dc):
+        if dc is None: return
+        dc = min(dc, 100) #A simple clamp
+        dc = max(dc, 0)
+        self.pwm.ChangeDutyCycle(dc)
     
     def brake(self):
         GPIO.output(self.pin_cntrl_1, GPIO.LOW)
         GPIO.output(self.pin_cntrl_2, GPIO.LOW)
-        
-    #def float(self):
-    #    self.state.update(Direction.FLOAT)
     
     def float(self):
         GPIO.output(self.pin_cntrl_1, GPIO.HIGH)
         GPIO.output(self.pin_cntrl_2, GPIO.HIGH)
 
-    #def forward(self, speed = None):
-    #    self.set_speed(speed)
-    #    self.state.update(Direction.FORWARD)
-
     def forward(self):
         GPIO.output(self.pin_cntrl_1, GPIO.HIGH)
         GPIO.output(self.pin_cntrl_2, GPIO.LOW)
-
-    #def reverse(self, speed = None):
-    #    self.set_speed(speed)
-    #    self.state.update(Direction.REVERSE)
 
     def reverse(self):
         GPIO.output(self.pin_cntrl_1, GPIO.LOW)
@@ -88,37 +64,83 @@ class MotorController:
         self.right_motor = Motor(PIN_R_PWM, PIN_R_CNTRL_1, PIN_R_CNTRL_2)
         self.left_motor = Motor(PIN_L_PWM, PIN_L_CNTRL_1, PIN_L_CNTRL_2)
 
-    def forward(self, speed = None):
+        self.left_speed = 1
+        self.right_speed = 1
+        self.speed = 100
+
+        self.direction = 0
+        self.forward_timestamp = monotonic()
+        self.reverse_timestamp = monotonic()
+
+    def update_state(self, new_dir):
+        if (self.direction == 1 and new_dir != 1):
+            self.forward_timestamp = monotonic()
+
+        elif (self.direction == -1 and new_dir != -1):
+            self.reverse_timestamp = monotonic()
+
+        self.direction = new_dir
+
+
+    def safe_brake_if_needed(self, new_dir):
+        self.update_state(new_dir)
+
+        now = monotonic()
+        if (new_dir == 1):
+            since_reverse = now - self.reverse_timestamp
+            if (since_reverse < SAFETY_BRAKE_TIME): sleep(SAFETY_BRAKE_TIME - since_reverse)
+        if (new_dir == -1):
+            since_forward = now - self.forward_timestamp
+            if (since_forward < SAFETY_BRAKE_TIME): sleep(SAFETY_BRAKE_TIME - since_forward)
+
+    def forward(self):
+        self.safe_brake_if_needed(1)
+
+        self.left_speed = 1
+        self.right_speed = 1
+
+        self.update_speed(self.speed)
+
         self.right_motor.forward()
         self.left_motor.forward()
 
-    def reverse(self, speed = None):
+    def reverse(self):
+        self.safe_brake_if_needed(-1)
+        
+        self.left_speed = 1
+        self.right_speed = 1
+
+        self.update_speed(self.speed)
+
         self.right_motor.reverse()
         self.left_motor.reverse()
 
-    def set_speed(self, speed):
-        self.right_motor.set_speed(speed)
-        self.left_motor.set_speed(speed)
+    def right(self):
+        self.left_speed = 1
+        self.right_speed = TURN_SLOW_WHEEL_SPEED_MULT
+        self.update_speed(self.speed)
+
+    def left(self):
+        self.left_speed = TURN_SLOW_WHEEL_SPEED_MULT
+        self.right_speed = 1
+        self.update_speed(self.speed)
+
+    def update_speed(self, speed):
+        self.speed = speed
+        self.right_motor.set_dc(self.right_speed * self.speed)
+        self.left_motor.set_dc(self.left_speed * self.speed)
 
     def brake(self):
+        self.update_state(0)
         self.right_motor.brake()
         self.left_motor.brake()
+        self.direction = 0
 
     def float(self):
+        self.update_state(0)
         self.right_motor.float()
         self.left_motor.float()
-
-    def right(self, speed=None):
-        self.right_motor.forward()
-        self.left_motor.reverse()
-
-    def left(self, speed=None):
-        self.right_motor.reverse()
-        self.left_motor.forward()
-
-    def update_both(self):
-        self.right_motor.update()
-        self.left_motor.update()
+        self.direction = 0
 
     def cleanup(self):
         self.right_motor.cleanup()
